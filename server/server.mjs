2f0bfff2a19276f32a745f58dec5afaf358d1eb7
to4sn1311 <vutoan131102@gmail.com>
2024-12-10 21:23:36 +0700

    ,

--- server/server.mjs
+++ server/server.mjs
@@ -1,15 +1,27 @@
 import express from "express";
 import http from "http";
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
 
 const app = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+
+const typeDefs = "";
+const resolvers = {};
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer: server })],
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer: httpServer })], 
 });
 
 await apolloServer.start();
+
+app.use(cors, bodyParser.json(), expressMiddleware(apolloServer));
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000`);
